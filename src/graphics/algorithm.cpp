93552b2099de299b78ea4541e64f7689e85bfb6f
cwood77 <dumuzi77@gmail.com>
2022-09-06 21:02:10 +0100

    added object finder impl (untested)

--- src/graphics/algorithm.cpp
+++ src/graphics/algorithm.cpp
@@ -1,3 +1,4 @@
+#include "../crayon/log.hpp"
 #include "algorithm.hpp"
 
 void frameRemover::run(iCanvas& c)
@@ -50,4 +51,93 @@ void frameRemover::markIn(const point& p)
 {
    m_canvas.setPixel(p,RGB(255,255,255));
    m_inFrame.insert(p);
+}
+
+void objectFinder::run(iCanvas& c, log& l)
+{
+   objectFinder self(c,l);
+   self._run();
+}
+
+objectFinder::objectFinder(iCanvas& c, log& l)
+: m_canvas(c)
+, m_log(l)
+, m_nextObjId(1)
+{
+   m_canvas.getDims(m_w,m_h);
+}
+
+void objectFinder::_run()
+{
+   for(long y=0;y<m_h;y++)
+   {
+      for(long x=0;x<m_w;x++)
+      {
+         auto col = m_canvas.getPixel(point(x,y));
+         if(col == RGB(255,255,255))
+            continue;
+
+         auto objId = findAdjacentMembership(point(x,y));
+         addToObject(point(x,y),objId);
+      }
+   }
+}
+
+size_t objectFinder::findAdjacentMembership(const point& p)
+{
+   size_t ans = 0;
+
+   if(p.x)
+   {
+      auto it = m_map.find(point(p.x-1,p.y));
+      if(it!=m_map.end())
+         ans = mergeObjectsIf(ans,it->second);
+   }
+   if(p.y)
+   {
+      auto it = m_map.find(point(p.x,p.y-1));
+      if(it!=m_map.end())
+         ans = mergeObjectsIf(ans,it->second);
+   }
+   if(p.x && p.y)
+   {
+      auto it = m_map.find(point(p.x-1,p.y-1));
+      if(it!=m_map.end())
+         ans = mergeObjectsIf(ans,it->second);
+   }
+   if(p.y)
+   {
+      auto it = m_map.find(point(p.x-1,p.y+1));
+      if(it!=m_map.end())
+         ans = mergeObjectsIf(ans,it->second);
+   }
+   return ans;
+}
+
+void objectFinder::addToObject(const point& p, size_t i)
+{
+   if(i == 0)
+   {
+      m_log.s().s() << "[find-object] found new object " << i << " starting at (" << p.x << "," << p.y << ")" << std::endl;
+      i = m_nextObjId++;
+   }
+   m_objects[i].insert(p);
+}
+
+size_t objectFinder::mergeObjectsIf(size_t oldObj, size_t newObj)
+{
+   if(oldObj == 0)
+      return newObj;
+
+   m_log.s().s() << "[find-object] merging objects " << oldObj << " <- " << newObj << std::endl;
+
+   // move all newObj's pnts to oldObj
+   std::set<point>& newPnts = m_objects[newObj];
+   for(auto p : newPnts)
+   {
+      m_map[p] = oldObj;
+      m_objects[oldObj].insert(p);
+   }
+   m_objects.erase(newObj);
+   return oldObj;
 }
--- src/graphics/algorithm.hpp
+++ src/graphics/algorithm.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "graphicsApi.hpp"
+#include <map>
 #include <set>
+
+class log;
 
 class frameRemover {
 public:
@@ -18,4 +21,26 @@ private:
    long m_h;
    std::set<point> m_inFrame;
    COLORREF m_frameColor;
+};
+
+class objectFinder {
+public:
+   static void run(iCanvas& c, log& Log);
+
+private:
+   objectFinder(iCanvas& c, log& l);
+
+   void _run();
+   size_t findAdjacentMembership(const point& p);
+   void addToObject(const point& p, size_t i);
+   size_t mergeObjectsIf(size_t oldObj, size_t newObj);
+
+   iCanvas& m_canvas;
+   log& m_log;
+   long m_w;
+   long m_h;
+
+   size_t m_nextObjId;
+   std::map<size_t,std::set<point> > m_objects;
+   std::map<point,size_t> m_map;
 };
